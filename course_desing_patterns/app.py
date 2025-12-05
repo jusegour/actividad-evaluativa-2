1395b4046deb2725d8d87ec6c36f699d6f6ae0b5
unknown <you@example.com>
2025-12-05 11:50:54 -0500

    JGO - Limpiamos codigo del archivo app. Corregimos los datos de la bd ya que habian 2 registros duplicados.

--- course_desing_patterns/app.py
+++ course_desing_patterns/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_restful import Api
+
+# Importamos los recursos refactorizados
 from endpoints.products import ProductsResource
 from endpoints.auth import AuthenticationResource
 from endpoints.categories import CategoriesResource
@@ -8,18 +10,11 @@ from endpoints.favorites import FavoritesResource
 app = Flask(__name__)
 api = Api(app)
 
-import json
-with open('db.json', 'r') as file:
-    products = json.load(file)
-
-api.add_resource( AuthenticationResource,'/auth')
-
+# Rutas
+api.add_resource(AuthenticationResource, '/auth')
 api.add_resource(ProductsResource, '/products', '/products/<int:product_id>') 
-
 api.add_resource(CategoriesResource, '/categories', '/categories/<int:category_id>')
-
 api.add_resource(FavoritesResource, '/favorites')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
